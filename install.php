580bae437ea6942b5f6157a6b8ae0008b300f16a
mtproo <96996066+mtproo@users.noreply.github.com>
2022-06-03 03:00:02 -0700

    Create install.php

---
+++ install.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+print 'Running ...';
+system('sudo apt-get install python3');
+system('clear');
+$secret = readline('secret : ');
+$port = readline('port : ');
+$tag = readline('tag : ');
+$tls = readline('tls : ');
+system('clear');
+print 'Running ...';
+system('curl -o MTProo.sh -L https://git.io/fjo34');
+exec("echo -e \"$port\nMTProo\n1\n$secret\nn\nn\n3\ny\n$tag\n$tls\n\n\" | bash MTProo.sh", $proxy);
+preg_match('~proxy?server=(?:[\w|\.\-]+)&port=\d+&secret=.+$~i', $proxy, $match);
+print 'Proxy : https://t.me/'.$match[0];
+system('sudo systemctl start mtprotoproxy');
+system('echo -e "2\ny" | bash MTProo.sh');
+system('echo -e "9\ny" | bash MTProo.sh');
+?>
