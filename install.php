879af3cfd5a6b2dd1309d28fef1453c580804c4e
mtproo <96996066+mtproo@users.noreply.github.com>
2022-06-03 03:46:51 -0700

    Update install.php

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(0);
+error_reporting(1);
 print 'Running ...';
 system('sudo apt-get install python3');
 system('clear');
@@ -12,7 +12,7 @@ print 'Running ...';
 try {
 system('curl -o MTProo.sh -L https://git.io/fjo34');
 $ok = 'echo -e "'.$port.'\nMTProo\n1\n'.$secret.'\nn\nn\n3\ny\n'.$tag.'\n'.$tls.'\n\n" | bash MTProo.sh';
-system($ok, $proxy);
+print($ok);
 preg_match('~proxy?server=(?:[\w|\.\-]+)&port=\d+&secret=.+$~i', $proxy, $match);
 print 'Proxy : https://t.me/'.$match[0];
 } catch (\Throwable $e) {
