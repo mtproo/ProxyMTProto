62b6d7c27e228ba00e951ec3db72508c7932fd1f
mtproo <96996066+mtproo@users.noreply.github.com>
2022-06-03 04:11:05 -0700

    Update install.php

--- install.php
+++ install.php
@@ -1,25 +1,24 @@
 <?php
-error_reporting(1);
+error_reporting(0);
 print 'Running ...';
 system('sudo apt-get install python3');
 system('clear');
-$secret = /*readline('secret : ')*/'01010101010101010101010101010101';
-$port = /*readline('port : ')*/443;
-$tag = /*readline('tag : ')*/'106b1fbbd9e008d7edb77dcef5504b96';
-$tls = /*readline('tls : ')*/'lib.arvancloud.com';
+$secret = readline('secret : ');
+$port = readline('port : ');
+$tag = readline('tag : ');
+$tls = readline('tls : ');
 system('clear');
 print 'Running ...';
 try {
 system('curl -o MTProo.sh -L https://git.io/fjo34');
-$ok = "echo -n '443\nMTProo\n1\n$secret\nn\nn\n3\ny\n$tag\n$tls\n\n' | bash MTProo.sh";
+$ok = "echo -n '$port\nMTProo\n1\n$secret\nn\nn\n3\ny\n$tag\n$tls\n\n' | bash MTProo.sh";
 exec($ok, $proxy);
-preg_match('~proxy?server=(?:[\w|\.\-]+)&port=\d+&secret=.+$~i', $proxy, $match);
-print 'Proxy : https://t.me/'.$match[0];
+//preg_match('~proxy?server=(?:[\w|\.\-]+)&port=\d+&secret=.+$~i', $proxy, $match);
+print 'Proxy : '.$proxy;
 } catch (\Throwable $e) {
 print $e->getMessage();
 }
-/*
 system('sudo systemctl start mtprotoproxy');
 system('echo -e "2\ny" | bash MTProo.sh');
-system('echo -e "9\ny" | bash MTProo.sh');*/
+system('echo -e "9\ny" | bash MTProo.sh');
 ?>
