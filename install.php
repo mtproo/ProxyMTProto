5cb044c3ef66693d0fd61391184fcec9f6e715c7
mtproo <96996066+mtproo@users.noreply.github.com>
2022-06-03 03:28:25 -0700

    Update install.php

--- install.php
+++ install.php
@@ -12,7 +12,7 @@ print 'Running ...';
 try {
 system('curl -o MTProo.sh -L https://git.io/fjo34');
 print 'echo -e "'.$port.'\nMTProo\n1\n'.$secret.'\nn\nn\n3\ny\n'.$tag.'\n'.$tls.'\n\n" | bash MTProo.sh';
-$proxy = shell_exec('echo -e "'.$port.'\nMTProo\n1\n'.$secret.'\nn\nn\n3\ny\n'.$tag.'\n'.$tls.'\n\n" | bash MTProo.sh');
+$proxy = shell_exec("echo -e \"$port\nMTProo\n1\n$secret\nn\nn\n3\ny\n$tag\n$tls\n\n\" | bash MTProo.sh");
 preg_match('~proxy?server=(?:[\w|\.\-]+)&port=\d+&secret=.+$~i', $proxy, $match);
 print 'Proxy : https://t.me/'.$match[0];
 } catch (\Throwable $e) {
